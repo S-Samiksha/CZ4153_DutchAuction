ba82f11846b8c91de179d1f56c051403213e6b27
S-Samiksha <samikshasankar01@gmail.com>
2023-11-08 23:46:59 +0800

    shell

---
+++ BackEnd_Contract/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+yarn hardhat node&
+yarn hardhat run scripts/calculate.js  --network localhost&
+
+wait 
+
+echo “Dutch Auction completed"
--- BackEnd_Contract/scripts/calculate.js
+++ BackEnd_Contract/scripts/calculate.js
@@ -4,21 +4,30 @@ let start = Date.now();
 async function myFunction(currentTime) {
   const { deployer, userOne, userTwo } = await getNamedAccounts();
   const Dutch_Auction = await ethers.getContract("Dutch_Auction", deployer);
-  console.log(`Got contract DA at ${Dutch_Auction.target}`);
-  transactionResponse = await Dutch_Auction.calculate();
-  currentPrice = await Dutch_Auction.retrieveCurrentPrice();
-  totalBidders = await Dutch_Auction.retrieveTotalBidder();
-  timeElapsed = (currentTime - start) / 1000;
-  unsoldAlgos = await Dutch_Auction.getUnsoldAlgos();
-
   console.log(
-    `Time Elapsed: %i minutes, Current price: %i, Total Bidders: %i, unsold tokens: %i`,
-    timeElapsed / 60,
-    currentPrice,
-    totalBidders,
-    unsoldAlgos
+    `----------------------------------------------------------------------------------------------------`
   );
-  console.log();
+  console.log(`Dutch Contract: ${Dutch_Auction.target}`);
+  auctionState = await Dutch_Auction.getAuctionState();
+  if (auctionState == 0) {
+    transactionResponse = await Dutch_Auction.calculate();
+    currentPrice = await Dutch_Auction.retrieveCurrentPrice();
+    totalBidders = await Dutch_Auction.retrieveTotalBidder();
+    timeElapsed = (currentTime - start) / 1000;
+    unsoldAlgos = await Dutch_Auction.getUnsoldAlgos();
+
+    console.log(
+      `Time Elapsed from contract deployment: ${
+        timeElapsed / 60
+      }, Current price: ${currentPrice}, Total Bidders: ${totalBidders}, unsold tokens: ${unsoldAlgos}`
+    );
+  } else {
+    console.log(`Auction is closed`);
+  }
+  console.log(
+    `----------------------------------------------------------------------------------------------------`
+  );
+  console.log(` `);
 }
 
 // main()
